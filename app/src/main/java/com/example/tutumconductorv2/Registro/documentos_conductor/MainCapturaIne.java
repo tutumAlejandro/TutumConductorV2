21194c9b0ca8a70921d06e1c239b629f5813e9de
Alejandro <apptutum2@gmail.com>
2021-05-22 16:42:37 -0500

    Actualizacion Camara

--- app/src/main/java/com/example/tutumconductorv2/Registro/documentos_conductor/MainCapturaIne.java
+++ app/src/main/java/com/example/tutumconductorv2/Registro/documentos_conductor/MainCapturaIne.java
@@ -1,13 +1,6 @@
 package com.example.tutumconductorv2.Registro.documentos_conductor;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
-
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -18,6 +11,11 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import com.example.tutumconductorv2.R;
 import com.example.tutumconductorv2.Registro.BD_registro.utilidades.cadenas_documentos;
@@ -40,15 +38,15 @@ public class MainCapturaIne extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_captura_ine);
+        if (ContextCompat.checkSelfPermission(MainCapturaIne.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainCapturaIne.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainCapturaIne.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1000);
+        }
 
         rol = getIntent().getStringExtra("rol");
 
         ine_frontal = findViewById(R.id.btn_frontal_ine);
 
         btn_retroceso_ine = findViewById(R.id.img_retroceso_ine);
-        if (ContextCompat.checkSelfPermission(MainCapturaIne.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainCapturaIne.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainCapturaIne.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1000);
-        }
 
         btn_retroceso_ine.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,50 +71,6 @@ public class MainCapturaIne extends AppCompatActivity {
         });
     }
 
-    //Metodo para crear un unico nombre a cada fotografia
-
-    String mCurrentPhotoPath;
-    private File createImageFile() throws IOException{
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName ="Backup"+timeStamp+"_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(imageFileName,".jpg",storageDir);
-
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-
-    //Metodo para tomar foto y crear el archivo
-    static final int REQUEST_TAKE_PHOTO = 1;
-    public void tomarFoto(View view){
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        if(takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            File photeFile = null;
-            try {
-             photeFile = createImageFile();
-            }catch (IOException ex){
-                //Error ocurrido al momento de tomar una fotografia
-            }
-            //continua si unicamente si el archivo fue creado satisfactoriamente
-            if(photeFile != null){
-                Uri photoURI = FileProvider.getUriForFile(this,"com.example.tutumconductorv2",photeFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,photoURI);
-                startActivityForResult(takePictureIntent,REQUEST_TAKE_PHOTO);
-            }
-        }
-    }
-
-    //Metodo para mostrar vista previa en un image view(image button) de la foto tomada
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            ine_frontal.setImageBitmap(imageBitmap);
-        }
-    }
 
     public void guardar_ine (View V)
     {
@@ -138,4 +92,48 @@ public class MainCapturaIne extends AppCompatActivity {
             finish();
         }
     }
+
+    String mCurrentPhotoPath;
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "Backup_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+    public void tomarFoto(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,"com.example.android.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ine_frontal.setImageBitmap(imageBitmap);
+        }
+    }
+
+
 }
